d09c72e5317582c2038cccd24e4800373648e56f
Ruva Sadya <ruvarashe.sadya@gmail.com>
2024-07-21 14:25:13 +0000

    Add files to augment and resize images

---
+++ resize_images.py
@@ -0,0 +1,34 @@
+import os
+from PIL import Image
+
+def resize_images(input_folder, output_folder, size=(224, 224)):
+    # Create the output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Loop through all files in the input folder
+    for filename in os.listdir(input_folder):
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
+            # Open the image
+            with Image.open(os.path.join(input_folder, filename)) as img:
+                # Resize the image
+                img_resized = img.resize(size, Image.LANCZOS)
+                
+                # Prepare the output filename
+                output_filename = f"resized_{filename.split('.')[0]}.png"
+                output_path = os.path.join(output_folder, output_filename)
+                
+                # Save the resized image
+                img_resized.save(output_path)
+                print(f"Resized {filename} to 224x224")
+
+# Usage
+input_folder = r"C:\Users\Ruvarashe\Desktop\Alumna Changemaker\Offiicial Selected Dataset\Zingiber Test Remove"
+output_folder = r"C:\Users\Ruvarashe\Desktop\Alumna Changemaker\Offiicial Selected Dataset\Zingiber Test Resized"
+
+print(f"Resizing images from: {input_folder}")
+print(f"Saving resized images to: {output_folder}")
+
+resize_images(input_folder, output_folder)
+
+print("Resizing complete!")
